0fe8e15d5cd0af3080da932930f8f10377e4bc0e
Huggywuggyurmu09 <ro.bloxdoo.rs897@gmail.com>
2025-07-04 20:37:03 +0100

    Create alpine.js

---
+++ uptime-kuma-installer/alpine.js
@@ -0,0 +1,32 @@
+// u can run this using harbor on pterodactyl or any alpine distro
+const { execSync } = require("child_process");
+const fs = require("fs");
+const path = require("path");
+
+const kumaRepo = "https://github.com/louislam/uptime-kuma.git";
+const kumaDir = path.join(__dirname, "uptime-kuma");
+const PORT = 5748; // Change this port if needed
+process.env.PORT = PORT.toString();
+
+console.log("[💾 1/6] Installing packages...");
+execSync(`apk update`, { stdio: "inherit" });
+execSync(`apk add --no-cache git nodejs npm python3 make g++ bash`, { stdio: "inherit" });
+
+if (!fs.existsSync(kumaDir)) {
+    console.log("[📩 2/6] Cloning Uptime Kuma...");
+    execSync(`git clone ${kumaRepo}`, { stdio: "inherit" });
+}
+process.chdir(kumaDir);
+console.log("[🔃 3/6] Resetting git repo...");
+execSync("git reset --hard && git clean -fd", { stdio: "inherit" });
+
+if (!fs.existsSync("node_modules")) {
+    console.log("[⌛ 4/6] Installing dependencies...");
+    execSync("npm install", { stdio: "inherit" });
+}
+
+console.log("[🏃‍♀️ 5/6] Running setup...");
+execSync("npm run setup", { stdio: "inherit" });
+
+console.log(`[✅ 6/6] Starting Uptime Kuma on port ${PORT}...`);
+require(path.join(__dirname, "uptime-kuma", "server", "server.js"));
